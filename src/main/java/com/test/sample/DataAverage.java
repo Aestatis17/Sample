87bbf9a974ea98e8a602659914822e956cf0b531
rafes <rafes@Yeet>
2021-02-26 10:24:22 -0500

    Old code from last time. I need to go back through and try to make it better (like what you showed me last time)... I might rewrite it

--- src/main/java/com/test/sample/DataAverage.java
+++ src/main/java/com/test/sample/DataAverage.java
@@ -1,5 +1,41 @@
 package com.test.sample;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataAverage {
+
+    public static void main(String[] args) throws IOException {
+
+        List<Integer> dataStorage = new ArrayList<>();
+
+        //Processes the file
+        File file = new File( "data.csv" );
+        FileReader fileReader = new FileReader( file );
+        BufferedReader reader = new BufferedReader( fileReader );
+
+        int counter = 0;
+        while ( reader.ready() ) {
+            String string = reader.readLine();
+            System.out.println( string );
+            if ( counter != 0 ) {
+                String[] data = string.split( "," );
+                String num = data[0].trim();
+                dataStorage.add( Integer.valueOf( num ) );
+            }
+            counter++;
+        }
+
+        // iterate over the list and sum the values
+        int sum = 0;
+        for (Integer num : dataStorage) {
+            sum += num;
+        }
+        System.out.println("sum = " + sum);
+        System.out.println( "Average: " + ( sum / dataStorage.size() ));
+        reader.close();
+
+    }
 
 }
